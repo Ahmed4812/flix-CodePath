88444a4f052afb8b6c5bfcd2118ad7341cd50d34
Ali Ahmed Khan <aliahmedkhan4812@gmail.com>
2022-02-25 23:13:48 -0500

    finished basic

--- flix/MoviesViewController.swift
+++ flix/MoviesViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Foundation
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     var movies = [[String:Any]]()
@@ -21,11 +22,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
         
-        cell.textLabel!.text = "Title: \(title)"
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+        cell.posterView.af.setImage(withURL: posterUrl!)
         return cell
     }
     
@@ -35,7 +44,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         
-        print("hello")
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -48,7 +56,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                     self.movies = dataDictionary["results"] as! [[String:Any]]
                  
                     self.tableView.reloadData()
-                    print(dataDictionary)
                         // TODO: Get the array of movies
                         // TODO: Store the movies in a property to use elsewhere
                         // TODO: Reload your table view data
